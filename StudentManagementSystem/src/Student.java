0042db0cbd87b5e9b9dbf7d88e112e59be3ad999
Michael Eshun <mykie.eshun@gmail.com>
2023-08-27 00:53:36 +0000

    Created a student object

---
+++ StudentManagementSystem/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String first_name;
+    String last_name;
+    int age;
+    char grade;
+
+    public Student(String first_name, String last_name, int age, char grade){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
